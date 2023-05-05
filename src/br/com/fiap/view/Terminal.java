55887616abc534e29b899a9298455db6fbab25aa
Asteriuz <gutocebola@gmail.com>
2023-05-05 18:11:15 -0300

    Updated Terminal.java

--- src/br/com/fiap/view/Terminal.java
+++ src/br/com/fiap/view/Terminal.java
@@ -1,134 +1,24 @@
 package br.com.fiap.view;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Image;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+
+import javax.swing.UIManager;
 
 import br.com.fiap.models.*;
 
 public class Terminal {
-    static boolean debug = true;
-    static Usuario usuario = null;
-    static ImageIcon icon = new ImageIcon("GFX/logo/logo.png");
+    static boolean debug = false;
 
-    public static Usuario login(SistemaBancario sb) {
-        while (true) {
-            Image image = icon.getImage();
-            Image newimg = image.getScaledInstance(80, 80, java.awt.Image.SCALE_SMOOTH); 
-            icon = new ImageIcon(newimg);
-
-            JPanel panel = new JPanel(new GridBagLayout());
-            panel.setPreferredSize(new java.awt.Dimension(200, 100));
-            GridBagConstraints gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 0;
-            gbc.anchor = GridBagConstraints.LINE_START;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            gbc.weightx = 1.0;
-            panel.add(new JLabel("Digite seu email:"), gbc);
-            gbc.gridy++;
-            JTextField textField = new JTextField(10);
-            textField.setPreferredSize(new java.awt.Dimension(200, 24));
-            panel.add(textField, gbc);
-            int result = JOptionPane.showOptionDialog(null, panel, "Banco FinHive",
-                    JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE,
-                    icon, new String[] { "OK", "Cadastrar", "Cancelar" }, "OK");
-
-            if (result == 2) {
-                System.exit(0);
-            }
-
-            if (result == 1) {
-                cadastrar(sb);
-            } else {
-
-                String email = textField.getText();
-                if (email == null) {
-                    System.exit(0);
-                }
-                String senha = (String) JOptionPane.showInputDialog(null, "Digite sua senha:", "Banco FinHive",
-                        JOptionPane.QUESTION_MESSAGE, icon, null, null);
-                if (senha == null) {
-                    System.exit(0);
-                }
-                usuario = sb.buscarUsuario(email);
-                if (usuario == null || !usuario.getSenha().equals(senha)) {
-                    JOptionPane.showMessageDialog(null, "Usuário ou senha inválidos");
-                    continue;
-                }
-                return usuario;
-            }
-        }
-    }
-
-    public static void cadastrar(SistemaBancario sb) {
-        while (true) {
-            Image image = icon.getImage(); 
-            Image newimg = image.getScaledInstance(80, 80, java.awt.Image.SCALE_SMOOTH); 
-            icon = new ImageIcon(newimg);
-
-            JPanel panel = new JPanel(new GridBagLayout());
-            panel.setPreferredSize(new java.awt.Dimension(200, 150));
-            GridBagConstraints gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 0;
-            gbc.anchor = GridBagConstraints.LINE_START;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            gbc.weightx = 1.0;
-            panel.add(new JLabel("Digite seu nome:"), gbc);
-            gbc.gridy++;
-            JTextField textField = new JTextField(10);
-            textField.setPreferredSize(new java.awt.Dimension(200, 24));
-            panel.add(textField, gbc);
-            gbc.gridy++;
-            panel.add(new JLabel("Digite seu email:"), gbc);
-            gbc.gridy++;
-            JTextField textField2 = new JTextField(10);
-            textField2.setPreferredSize(new java.awt.Dimension(200, 24));
-            panel.add(textField2, gbc);
-            gbc.gridy++;
-            panel.add(new JLabel("Digite sua senha:"), gbc);
-            gbc.gridy++;
-            JTextField textField3 = new JTextField(10);
-            textField3.setPreferredSize(new java.awt.Dimension(200, 24));
-            panel.add(textField3, gbc);
-            int result = JOptionPane.showOptionDialog(null, panel, "Banco FinHive",
-                    JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE,
-                    icon, new String[] { "OK", "Cancelar" }, "OK");
-
-            if (result == 1) {
-                System.exit(0);
-            }
-
-            String nome = textField.getText();
-            String email = textField2.getText();
-            String senha = textField3.getText();
-            if (nome == null || email == null || senha == null) {
-                System.exit(0);
-            }
-            if (sb.buscarUsuario(email) != null) {
-                JOptionPane.showMessageDialog(null, "Email já cadastrado");
-                continue;
-            }
-            sb.addUsuario(
-                    new Usuario(nome, email, senha, sb.createNewContaCorrente(), sb.createNewContaPoupanca(), "GFX/profiles/defaultAvatar.png"));
-            JOptionPane.showMessageDialog(null, "Usuário cadastrado com sucesso");
-            return;
-        }
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        UIManager.put("Button.defaultButtonFollowsFocus", Boolean.TRUE);
+        Usuario usuarioLogado = null;
 
         SistemaBancario sb = new SistemaBancario();
 
@@ -169,7 +59,11 @@ public class Terminal {
 
         // sb.saveData();
 
-        String filePath = "data.txt";
+        String filePath = "./data.txt";
+        try (InputStream in = Terminal.class.getResourceAsStream("/data.txt");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+            // Use resource
+        }
         List<List<String>> data = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
@@ -194,38 +88,38 @@ public class Terminal {
         /* -------------------------------------------------------------------------- */
 
         if (debug) {
-            usuario = sb.getUsuarios().get(0);
+            usuarioLogado = sb.getUsuarios().get(0);
         } else {
-            usuario = login(sb);
+            usuarioLogado = Interface.login(sb);
         }
 
         String menu = "1 - Depositar\n2 - Sacar\n3 - Transferir\n4 - Exibir Transações\n5 - Emitir/Pagar Boleto\n6 - Investimento\n7 - Serviço Assessoria\n8 - Trocar Conta\n9 - Sair\n\nEscolha uma opção:";
         while (true) {
-            int opcao = Interface.menu(menu, usuario);
+            int opcao = Interface.menu(menu, usuarioLogado);
             switch (opcao + 1) {
                 case 1:
-                    Interface.depositar(usuario);
+                    Interface.depositar(usuarioLogado);
                     break;
                 case 2:
-                    Interface.sacar(usuario);
+                    Interface.sacar(usuarioLogado);
                     break;
                 case 3:
-                    Interface.transferir(usuario, sb);
+                    Interface.transferir(usuarioLogado, sb);
                     break;
                 case 4:
-                    Interface.relatorio(usuario);
+                    Interface.relatorio(usuarioLogado);
                     break;
                 case 5:
-                    Interface.boleto(usuario, sb);
+                    Interface.boleto(usuarioLogado, sb);
                     break;
                 case 6:
-                    Interface.investir(usuario, sb);
+                    Interface.investir(usuarioLogado, sb);
                     break;
                 case 7:
-                    Interface.assessoria(usuario, sb);
+                    Interface.assessoria(usuarioLogado, sb);
                     break;
                 case 8:
-                    usuario = login(sb);
+                    usuarioLogado = Interface.login(sb);
                     break;
                 case 9:
                     sb.saveData();
